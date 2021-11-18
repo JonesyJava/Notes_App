c404ddaf377d94b5b36f20f1d85feb9003ebf1a6
jonesyjava <jonesyjava@gmail.com>
2021-11-18 10:58:42 -0700

    hidden element toggle added

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,17 +2,24 @@
   <div class="d-flex justify-content-center">
     <div class="notes">
       <div class="tools d-flex justify-content-end">
-        <button class="fa fa-pencil"></button>
+        <button class="fa fa-pencil edit"></button>
          <button class="fa fa-trash"></button>
       </div>
+        <div class="best hidden">
         <textarea></textarea>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup(){
+
+    return{}
+  }
 }
 </script>
 
@@ -28,6 +35,29 @@ export default {
 background-color: #9ec862;
 justify-content: flex-end !important;
 padding: 0.5rem;
+}
+
+.notes .tools button{
+  background-color: transparent;
+  border: none;
+  color: #fff;
+  font-size: 1;
+  margin-left: 0.5rem;
+}
+.notes .best{
+  height: 100%;
+  width: 100%;
+}
+
+.notes textarea{
+  font-family: Arial, Helvetica, sans-serif;
+  border: none;
+  height: 100%;
+  width: 100%;
+}
+
+.notes .hidden{
+  display: none;
 }
 
 </style>
--- src/services/NotesService.js
+++ src/services/NotesService.js
@@ -1,6 +1,19 @@
 
+const notesEl = document.querySelector(".note")
+const editBtn = document.querySelector(".edit")
+const deleteBtn = document.querySelector(".delete")
+const bestEl = notesEl.querySelector(".best")
+const textArea = notesEl.querySelector("textarea")
 
 class NotesService{
+
+  hiddenBtn(){
+    editBtn.addEventListener('click', () => {
+      bestEl.classList.toggle('hidden')
+      textArea.classList.toggle('hidden')
+    })
+
+  }
 
 }
 
